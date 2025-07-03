8b3319807f58124189d642bcbdac623d571f9564
Anmol Sah <kumarxxx242424@gmail.com>
2025-07-03 22:14:55 +0530

    Fix typo in className and import Navigation component

--- movieco/src/App.jsx
+++ movieco/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div classNmae="">
-
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900">
+      <Navigation />
     </div>
-  )
+  );
 }
 
 export default App;
